74d9380aefe0e4bc8d874dbc7c70c73517dc9b1a
iraheem04 <144250576+iraheem04@users.noreply.github.com>
2024-07-24 22:08:30 -0400

    first commit

---
+++ #assignment 1.py
@@ -0,0 +1,12 @@
+import math
+# input1=input("please enter number x: ")
+# input2=input("please enter number y: ")
+# x=int(input1)
+# y=int(input2)
+# print(x**y)
+# print (math.log2(x))
+
+x = int(input("enter a number: "))
+y = int(input("enter another number: "))
+print(x**y)
+print(math.log2(x))
---
+++ notes.py
@@ -0,0 +1,19 @@
+# ## abstracting expersions
+# pi = 3.14
+# radius = 2.2
+# area= pi*(radius**2)
+
+# ##concatenating strings
+
+# hi = "hello there "
+# name = "ana"
+# greeting =hi + name
+# # print (greeting*3) # the "*" allowws you to print strings x times
+
+##
+
+x=1
+print(x)
+x_str=str(x)
+print ("my fav num is",x,".","x=",x)
+print ("my fav num is" + x_str+ "x="+x_str)
